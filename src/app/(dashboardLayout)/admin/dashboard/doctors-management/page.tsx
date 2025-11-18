d693e7a06956202cbf7565bcb1060aad81146613
Rakib Hossain <hossainrokib55@gmail.com>
2025-11-18 22:17:23 +0600

    admin:doctor,specialites

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import DoctorsManagementHeader from "@/components/modules/admin/DoctorsManagement/DoctorsManagementHeader";
+import DoctorsTable from "@/components/modules/admin/DoctorsManagement/DoctorsTable";
+import RefreshButton from "@/components/shared/RefreshButton";
+import SearchFilter from "@/components/shared/SearchFilter";
+import SelectFilter from "@/components/shared/SelectFilter";
+import TablePagination from "@/components/shared/TablePagination";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/lib/formatters";
+import { getDoctors } from "@/services/admin/doctorManagement";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+import { ISpecialty } from "@/types/specialities.interface";
+import { Suspense } from "react";
 
-function doctorManagementPage() {
-  return <div>doctorManagementPage</div>;
-}
+const AdminDoctorsManagementPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const searchParamsObj = await searchParams;
+  const queryString = queryStringFormatter(searchParamsObj); // {searchTerm: "John", speciality: "Cardiology" => "?searchTerm=John&speciality=Cardiology"}
 
-export default doctorManagementPage;
+  const specialitiesResult = await getSpecialities();
+  const doctorsResult = await getDoctors(queryString);
+  const totalPages = Math.ceil(
+    doctorsResult.meta.total / doctorsResult.meta.limit
+  );
+  return (
+    <div className="space-y-6">
+      <DoctorsManagementHeader specialities={specialitiesResult.data} />
+      <div className="flex space-x-2">
+        <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
+        <SelectFilter
+          paramName="speciality" // ?speciality="Cardiology"
+          options={specialitiesResult.data.map((speciality: ISpecialty) => ({
+            label: speciality.title,
+            value: speciality.title,
+          }))}
+          placeholder="Filter by speciality"
+        />
+        <RefreshButton />
+      </div>
+      <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
+        <DoctorsTable
+          doctors={doctorsResult.data}
+          specialities={specialitiesResult.data}
+        />
+        <TablePagination
+          currentPage={doctorsResult.meta.page}
+          totalPages={totalPages}
+        />
+      </Suspense>
+    </div>
+  );
+};
+
+export default AdminDoctorsManagementPage;
--- src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import SpecialitiesManagementHeader from "@/components/modules/admin/SpecialitiesManagement/SpecialitiesManagementHeader";
+import SpecialitiesTable from "@/components/modules/admin/SpecialitiesManagement/SpecialitiesTable";
+import RefreshButton from "@/components/shared/RefreshButton";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+import { Suspense } from "react";
 
-function specialtiesManagementPage() {
-  return <div>specialties Management Page</div>;
-}
 
-export default specialtiesManagementPage;
+const AdminSpecialitiesManagementPage = async () => {
+  const result = await getSpecialities();
+  return (
+    <div className="space-y-6">
+      <SpecialitiesManagementHeader />
+      <div className="flex">
+        <RefreshButton />
+      </div>
+      <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
+        <SpecialitiesTable specialities={result.data} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default AdminSpecialitiesManagementPage;
